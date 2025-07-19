5a91687d6717fc8ce5c606b796762ab82c573654
saurabh <saurabhverma2693@gmail.com>
2025-07-19 18:02:13 +0530

    Forward Props to wrapped Elements

--- CoreConcepts/src/components/Example.jsx
+++ CoreConcepts/src/components/Example.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 import { EXAMPLES } from "../data"; 
 
+import Section from "./Section.jsx";
+
 import TabButton from "./TabButton.jsx"
 
 export default function Examples(){
@@ -28,29 +30,29 @@ export default function Examples(){
     
 return (
     
-            <section id="examples">
-              <h1>Examples</h1>
+            <Section title="Examples" id="examples">
+              
               <menu>
                 <TabButton 
                 isSelected={selectedTopic === 'components'} 
-                onSelect={() => handleSelect("components")}>
+                onClick={() => handleSelect("components")}>
                   Components
                 </TabButton>
                 <TabButton
                 isSelected={selectedTopic === 'jsx'}  
-                onSelect={() => handleSelect("jsx")}
+                onClick={() => handleSelect("jsx")}
                 >JSX</TabButton>
                 <TabButton
                 isSelected={selectedTopic === 'props'} 
-                onSelect={() => handleSelect("props")}
+                onClick={() => handleSelect("props")}
                 >Props</TabButton>
                 <TabButton
                 isSelected={selectedTopic === 'state'} 
-                onSelect={() => handleSelect("state")}
+                onClick={() => handleSelect("state")}
                 >State</TabButton>
               </menu>
                {tabContent}
-            </section>
+            </Section>
 );
 }
 
